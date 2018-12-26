2c483d15a62584417f57a585f5908c8ab6896497
kmpujar <karthikpujar737@gmail.com>
2018-12-26 01:33:11 -0500

    Init Commit

---
+++ encdec.py
@@ -0,0 +1,143 @@
+import nltk
+import numpy as np
+import pandas as pd
+import unicodedata
+from collections import defaultdict
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import LSTM
+from keras.callbacks import ModelCheckpoint
+from keras.utils import np_utils
+
+from numpy import array
+from pickle import dump
+from keras.preprocessing.text import Tokenizer
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Embedding
+
+from random import randint
+from pickle import load
+from keras.models import load_model
+from keras.preprocessing.sequence import pad_sequences
+
+
+# reviews= pd.read_csv("yelp.csv", usecols=['text'])
+# np.savetxt(r'reviews.txt', reviews.values, fmt='%s')
+# with open('reviews.txt', 'r') as myfile:
+#   data = myfile.read()
+# asciitext=unicodedata.normalize('NFKD', data).encode('ascii','ignore')
+#
+# text_file = open("reviews_ascii.txt", "w")
+# text_file.write(str(asciitext))
+# text_file.close()
+
+def load_doc(filename):
+	file = open(filename, 'r')
+	text = file.read()
+	file.close()
+	return text
+
+def clean_doc(text):
+	tokens = nltk.word_tokenize(text)
+	tokens = [word.lower() for word in tokens]
+	return tokens
+
+def save_doc(lines, filename):
+	data = '\n'.join(lines)
+	file = open(filename, 'w')
+	file.write(data)
+	file.close()
+
+filename = 'reviews.txt'
+doc = load_doc(filename)
+tokens = clean_doc(doc)
+
+print('Total Tokens: %d' % len(tokens))
+print('Unique Tokens: %d' % len(set(tokens)))
+
+length = 5 + 1
+sequences = list()
+for i in range(length, len(tokens)):
+	seq = tokens[i-length:i]
+	line = ' '.join(seq)
+	sequences.append(line)
+print('Total Sequences: %d' % len(sequences))
+
+out_filename = 'reviews_sequences.txt'
+save_doc(sequences, out_filename)
+
+#Read sequences
+in_filename = 'reviews_sequences.txt'
+doc = load_doc(in_filename)
+lines = doc.split('\n')
+#Encoding using KEras tokenizer
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(lines)
+sequences = tokenizer.texts_to_sequences(lines)
+vocab_size = len(tokenizer.word_index) + 1
+# separate into input and output
+sequences = array(sequences)
+X, y = sequences[:,:-1], sequences[:,-1]
+y = to_categorical(y, num_classes=vocab_size)
+seq_length = X.shape[1]
+# define model
+model = Sequential()
+model.add(Embedding(vocab_size, 5, input_length=seq_length))
+model.add(LSTM(100, return_sequences=True))
+model.add(LSTM(100))
+model.add(Dense(100, activation='relu'))
+model.add(Dense(vocab_size, activation='softmax'))
+print(model.summary())
+# compile model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+# fit model
+model.fit(X, y, batch_size=128, epochs=100)
+# save the model to file
+model.save('model.h5')
+# save the tokenizer
+dump(tokenizer, open('tokenizer.pkl', 'wb'))
+
+
+# generate a sequence from a language model
+def generate_seq(model, tokenizer, seq_length, seed_text, n_words):
+	result = list()
+	in_text = seed_text
+	# generate a fixed number of words
+	for _ in range(n_words):
+		# encode the text as integer
+		encoded = tokenizer.texts_to_sequences([in_text])[0]
+		# truncate sequences to a fixed length
+		encoded = pad_sequences([encoded], maxlen=seq_length, truncating='pre')
+		# predict probabilities for each word
+		yhat = model.predict_classes(encoded, verbose=0)
+		# map predicted word index to word
+		out_word = ''
+		for word, index in tokenizer.word_index.items():
+			if index == yhat:
+				out_word = word
+				break
+		# append to input
+		in_text += ' ' + out_word
+		result.append(out_word)
+	return ' '.join(result)
+
+in_filename = 'reviews_sequences.txt'
+doc = load_doc(in_filename)
+lines = doc.split('\n')
+seq_length = len(lines[0].split()) - 1
+
+# load the model
+model = load_model('model.h5')
+
+# load the tokenizer
+tokenizer = load(open('tokenizer.pkl', 'rb'))
+
+# select a seed text
+seed_text = lines[randint(0,len(lines))]
+print(seed_text + '\n')
+
+# generate new text
+generated = generate_seq(model, tokenizer, seq_length, seed_text, 5)
+print(generated)
